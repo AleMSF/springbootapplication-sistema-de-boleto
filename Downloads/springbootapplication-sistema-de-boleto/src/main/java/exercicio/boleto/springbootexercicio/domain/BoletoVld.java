104ece0272254acea216dc11a334bf0b54fe74cf
Alessandro Souza <121178363+AleMSF@users.noreply.github.com>
2023-01-25 16:05:35 -0300

    Delete Downloads/springbootapplication-sistema-de-boleto directory

--- Downloads/springbootapplication-sistema-de-boleto/src/main/java/exercicio/boleto/springbootexercicio/domain/BoletoVld.java
+++
@@ -1,5 +0,0 @@
-package exercicio.boleto.springbootexercicio.domain;
-
-public enum BoletoVld {
-    VENCIDO, PAGO, A_PAGAR
-}
--- Downloads/springbootapplication-sistema-de-boleto/src/main/java/exercicio/boleto/springbootexercicio/repository/BoletoRepository.java
+++
@@ -1,8 +0,0 @@
-package exercicio.boleto.springbootexercicio.repository;
-
-import exercicio.boleto.springbootexercicio.domain.Boleto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoletoRepository extends JpaRepository<Boleto, Integer> {
-
-}
